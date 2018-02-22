84d54c5685ee65efdcdb8972b0bd56cfc3f7b64a
dragonmaster10 <simonaantica@gmail.com>
2018-02-22 18:01:36 +0000

    initial commit

---
+++ src/main/java/com/dragonmaster10/musicAndLifeProgram/App.java
@@ -0,0 +1,13 @@
+package com.dragonmaster10.musicAndLifeProgram;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
